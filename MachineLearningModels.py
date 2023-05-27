34605731edb03cc0a402684880d6c3d52e17279c
Yash Patel <ykp1997@gmail.com>
2023-05-27 08:48:59 -0700

    adding random forest starter code

--- MachineLearningModels.py
+++ MachineLearningModels.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import RandomForestRegressor
 import xgboost as xgb
 import tensorflow as tf
 import pandas as pd
@@ -13,18 +15,28 @@ def main():
     # The target will be the adjusted close price of NEXT week, to train the ML models on predicting future stock price
     target = data['Adjusted Close'][1:]
 
+    # split the data (80% train, 20% test)
+
+
+    # Train the models
+    random_forest_model = pyspark_random_forest(features, target)
+    gradient_boosting_model = xgb_gradient_boosting(features, target)
+    neural_network_model = tf_neural_network(features, target)
 
 
 
+def pyspark_random_forest(features, target):
+
+    spark = SparkSession.builder\
+        .appName("RandomForestModel")\
+        .getOrCreate()
 
 
-def pyspark_random_forest():
+
+def xgb_gradient_boosting(features, target):
     pass
 
-def xgb_gradient_boosting():
-    pass
-
-def tf_neural_network():
+def tf_neural_network(features, target):
     pass
 
 if __name__ == '__main__':
